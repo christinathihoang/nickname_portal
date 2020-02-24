5c25e81db997d0b49514ec466bcdfd151f28655c
Christina Hoang <christinahoang@CHRISTINAHOANG.local>
2020-02-23 19:45:16 -0500

    login get post working

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField
 
 class LoginForm(FlaskForm):
   username = StringField('Username') #need to input DataRequired() validator later, DataRequired having issues with being imported 
   password = StringField('Password')
+  remember_me = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from app import app
 from app.forms import LoginForm, NicknameSubmissionForm
 
 @app.route('/')
-
-@app.route('/index')
 def index():
+  return "Kappa Submisson Portal"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
   form = LoginForm()
+  if form.validate_on_submit():   #if form is empty, returns false and renders template
+    flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+    return redirect('/')
   return render_template('login.html', form=form)
 
 @app.route('/submission')
