77aeab1c7a10060d909d4830eea2aecad0f988d5
Christina Hoang <christinahoang@CHRISTINAHOANG.local>
2020-03-28 02:53:37 -0400

    add stylization to login page

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,15 +1,7 @@
 // event listener for side nav
 document.addEventListener('DOMContentLoaded', function() {
   let elem = document.querySelector('.sidenav');
-  let instance = M.Sidenav.init(elem, {
-  });
-  // document.getElementById('menu-icon').addEventListener('click', function() {
-  //   if (instance.isOpen) {
-  //     instance.close();
-  //   }
-  //   else {
-  //     instance.open()
-  //   }
-  // });
+  let options = {};
+  let instance = M.Sidenav.init(elem, options);
 });
 
