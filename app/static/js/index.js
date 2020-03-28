ccb256c0c1309830a329b52792d2911058dce528
Christina Hoang <christinahoang@CHRISTINAHOANG.local>
2020-03-28 04:00:57 -0400

    stylize submission page with card with tabs

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -5,3 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
   let instance = M.Sidenav.init(elem, options);
 });
 
+
+// for submission.html
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelector('.tabs');
+  let options = {swipeable: true};
+  let instance = M.Tabs.init(elems, options);
+})
+
