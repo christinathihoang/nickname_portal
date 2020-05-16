c3e4d571cd11b2730a30e7557f1b5445e99f46cd
christinathihoang <christinahoang@utexas.edu>
2020-05-16 18:11:30 -0400

    restylize login page

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,15 +1,21 @@
-// event listener for side nav
-document.addEventListener('DOMContentLoaded', function() {
-  let elem = document.querySelector('.sidenav');
-  let options = {};
-  let instance = M.Sidenav.init(elem, options);
-});
+// // event listener for side nav
+// document.addEventListener('DOMContentLoaded', function() {
+//   let elem = document.querySelector('.sidenav');
+//   let options = {};
+//   let instance = M.Sidenav.init(elem, options);
+// });
 
 
-// for submission.html
-document.addEventListener('DOMContentLoaded', function() {
-  let elems = document.querySelector('.tabs');
-  let options = {swipeable: true};
-  let instance = M.Tabs.init(elems, options);
+// // for submission.html
+// document.addEventListener('DOMContentLoaded', function() {
+//   let elems = document.querySelector('.tabs');
+//   let options = {swipeable: true};
+//   let instance = M.Tabs.init(elems, options);
+// })
+
+// event listener to flip login/register card
+document.querySelector('.flip-card').addEventListener("click", function(e) {
+  e.preventDefault();
+  let card = document.querySelector('.card-body')
+  document.querySelector('.card-body').classList.toggle('flipped');
 })
-
